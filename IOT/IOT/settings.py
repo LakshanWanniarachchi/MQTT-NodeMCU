bcc2c2090ca3032f5067a1d0fcb3e7f1b6a4fe47
Lakshan Lakshitha <134108423+LakshanWanniarachchi@users.noreply.github.com>
2024-12-31 11:59:26 +0530

    change setting.py

--- IOT/IOT/settings.py
+++ IOT/IOT/settings.py
@@ -138,3 +138,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
